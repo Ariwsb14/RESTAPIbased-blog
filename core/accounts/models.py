3ff3ca12ed726c899fd5258c525d5d6ee5c715aa
Ariwsb14 <Ariasb1385@gmail.com>
2025-01-23 12:37:12 +0330

    making user model and Usermanager
    +no migrations

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,52 @@
 from django.db import models
-
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+'''
+Custum UserManger  for User MOdel
+'''
+class UserManager(BaseUserManager):
+    def CreateUser(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError(_('Email must be provided'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def Create_SuperUser(self,email,password,**extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('if_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True'))
+        
+        return self.CreateUser(email, password, **extra_fields)
+
+
+
+
+'''
+define CustomUser model for app with a unique email field and usermanager
+'''
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=250,unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    #is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+    def __str__(self):
+        return self.email
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
 # Create your models here.
+
+
+# defining calsses for the post of blogs
+
+class Post(models.Model):
+    # model for Post db
+    image = models.ImageField(null=True , blank=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now=True)
+    #author = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL,null=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+    
+
+# category class for blog posts
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
